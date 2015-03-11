9c7bda218b5a6ebc083722c1d00b6c924d9c1e0e
nelson.barbosa <nelson.barbosa@umb.edu.co>
2015-03-11 12:52:51 -0500

    pdf
    pdf

--- application/views/administracion/pesv_pdf.php
+++ application/views/administracion/pesv_pdf.php
@@ -1,37 +1,37 @@
 <div style="color">
     <b>Nombre de la empresa:</b> <?php echo $empresa[0]->emp_razonSocial; ?>
-    <br><b>LOGO DE LA EMPRESA:</b> 
-    <?php if (!empty($empresa[0]->userfile)) {
-        ?>
-        <center><img src="<?php echo base_url('/uploads/') . "/" . $empresa[0]->userfile ?>" style="width: 100px"></center>
-        <?php }
+    <!--<br><b>LOGO DE LA EMPRESA:</b>--> 
+    <?php // if (!empty($empresa[0]->userfile)) {
+    ?>
+        <!--<center><img src="<?php echo base_url('/uploads/') . "/" . $empresa[0]->userfile ?>" style="width: 100px"></center>-->
+    <?php // }
     ?>
     <h2>INTRODUCCIÓN</h2>
     <?php
     if (!empty($introduccion[0]['int_introduccion'])) {
-        echo "<p>" . $introduccion[0]['int_introduccion'] . "</p>";
+        echo "<p class='color'>" . $introduccion[0]['int_introduccion'] . "</p>";
     }
-    ?><h2>OBJETIVOS</h2>
+    ?>
+    <h2>OBJETIVOS</h2>
 </div>
 <h3>Objetivos generales</h3>
 <?php
 foreach ($general as $gen):
     ?>
-    <p><?php echo $gen['objGen_objetivo'] ?></p>
-    <?php
-endforeach;?>
-    <h3>Objetivos especificos</h3>
+    <p><?php echo utf8_encode($gen['objGen_objetivo']) ?></p>
+    <?php endforeach; ?>
+<h3>Objetivos especificos</h3>
 <?php foreach ($especificos as $esp) { ?>
-    <p><?php echo $esp['objEsp_objetivo'] ?></p><br>
-    <?php } ?>
-    
+    <p><?php echo utf8_encode($esp['objEsp_objetivo']) ?></p><br>
+<?php } ?>
+
 <h3>Objetivos por linea de accion del PESV</h3>
 <ul>
     <?php
     foreach ($lineaaccion as $accion => $objetivo) {
         echo "<li><b>" . $accion . "</b></li><ul>";
         foreach ($objetivo as $posicion => $obj) {
-            echo "<li>" . $obj . "</li>";
+            echo "<li>" . utf8_encode($obj) . "</li>";
         }
         echo "</ul><br>";
     }
@@ -52,8 +52,7 @@ endforeach;
 <p><?php echo $textomiembro[0]['comTex_texto']; ?></p>
 
 <h2>RESPONSABLE</h2>
-<p>Como responsable del PESV se a delegado por la Alta gerencia a: </p><?php
-foreach ($responsables as $responsable) : ?>
+<p>Como responsable del PESV se a delegado por la Alta gerencia a: </p><?php foreach ($responsables as $responsable) : ?>
     <p>
         <label>Nombre: </label><?php echo $responsable['res_cargo']; ?><br>
         <label>Cargo: </label><?php echo $responsable['res_nombre']; ?>
@@ -68,25 +67,26 @@ endforeach;
         <label>Nombre: </label><?php echo $comite['com_nombre']; ?><br>
         <label>Cargo: </label><?php echo $comite['com_cargo']; ?>
     </p>
- <?php } ?>
+<?php } ?>
 
 <p><?php echo $consultatextocomite[0]['texCom_texto']; ?></p>
 <h2>POLITICA</h2>
 <br>POLÍTICA  DE  SEGURIDAD  VIAL<?php
 if (!empty($politicas[0]['pol_politica'])) {
-    echo "<p>".$politicas[0]['pol_politica']."</p>"; } ?>
+    echo "<p>" . $politicas[0]['pol_politica'] . "</p>";
+}
+?>
 <br>
 <h2>DIAGNOSTICO</h2>
 <p><?php echo $diagnostico[0]['texDia_texto']; ?></p>
 <br>
 <h2>PRIORIDADES</h2>
-IDENTIFICACIÓN DE PRIORIDADES DE RIESGOS VIALES EN LA ORGANIZACIÓN <?php
-foreach ($prioridades as $prioridad) { ?>
+IDENTIFICACIÓN DE PRIORIDADES DE RIESGOS VIALES EN LA ORGANIZACIÓN <?php foreach ($prioridades as $prioridad) { ?>
     <p>
         <label>No Prioridad: </label><?php echo $prioridad['pri_prioridad']; ?> <br>
         <label>Nombre del Riesgo: </label><?php echo $prioridad['pri_riesgo']; ?>
     </p><br>
-    <?php } ?>
+<?php } ?>
 <style>
     h2{
         text-align: center;
